e0f0585934d1cbe262842c49f3d1dd464eaa1120
erick260490 <erkot90@gmail.com>
2017-05-05 15:02:47 -0500

    Se modifica controller para recibir objeto que contiene callback, se agregan tambien estilos que agrega Delia

--- classic/src/view/Erick/Erick.js
+++ classic/src/view/Erick/Erick.js
@@ -10,7 +10,7 @@ Ext.define('Ice.Erick.Erick', {
         'Ext.Button'
     ],
     controller: 'erickCntlr',
-    cls:        'sencha-dash-viewport',
+    cls:        'eck-cls',
     items:      [
 //        {
 //            xtype:   'Ext.data.Store',
